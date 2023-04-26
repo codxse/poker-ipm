58d06f1b7ad10f2c1bc86b5e1251b5533ca0cba8
Nadiar <codxse@gmail.com>
2023-04-26 21:49:37 +0700

    missing running port

--- socket/src/main.ts
+++ socket/src/main.ts
@@ -5,7 +5,6 @@ import { ValidationPipe } from '@nestjs/common'
 async function bootstrap() {
   const app = await NestFactory.create(RootModule)
   app.useGlobalPipes(new ValidationPipe())
-
-  return app
+  await app.listen(5000)
 }
 bootstrap()
