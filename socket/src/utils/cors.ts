808d06b72cdd1df6eb06beed0c45d100e4a101c3
Nadiar <codxse@gmail.com>
2023-08-12 16:32:23 +0700

    update cors regex

--- socket/src/utils/cors.ts
+++ socket/src/utils/cors.ts
@@ -1,6 +1,6 @@
 import { CorsOptions } from "@nestjs/common/interfaces/external/cors-options.interface";
 
 export const corsOptions: CorsOptions = {
-  origin: [/\.localhost:3000$/, /\.localhost:5000$/, /\.ipm\.poker$/],
+  origin: [/(.*)localhost:([0-9]*)$/, /(.*)\.?poker\.ipm$/],
   credentials: true,
 }
