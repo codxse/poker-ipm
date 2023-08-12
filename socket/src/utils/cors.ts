d25e64a8b35a22772cc48888e632ae1ef7ae59cc
Nadiar <codxse@gmail.com>
2023-08-12 16:55:58 +0700

    update regex cors and origin

--- socket/src/utils/cors.ts
+++ socket/src/utils/cors.ts
@@ -3,15 +3,7 @@ import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.int
 export const corsOptions: CorsOptions = {
   origin: [
     /(.*)localhost:([0-9]*)$/,
-    /(.*)\.?poker\.ipm$/,
-    'http://localhost:3000',
-    'http://localhost:5000',
-    'http://api.poker.ipm',
-    'https://api.poker.ipm',
-    'http://poker.ipm',
-    'https://poker.ipm',
-    'ws://api.poker.ipm',
-    'wss://api.poker.ipm',
+    /(.*)\.?ipm\.poker$/,
   ],
   credentials: true,
   methods: ['GET', 'POST', 'OPTIONS'],
